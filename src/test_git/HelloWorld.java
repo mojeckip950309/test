af67c015d01a0db22540224f2067c62b87b3fac5
mojeckip950309 <mojeckipiotr2@gmail.com>
2020-08-15 18:02:13 +0200

    dodanie zmiennej

--- src/test_git/HelloWorld.java
+++ src/test_git/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World!");
+		
+		int zmienna = 13;
+		System.out.println("Zmienna: " + zmienna);
 	}
 }
